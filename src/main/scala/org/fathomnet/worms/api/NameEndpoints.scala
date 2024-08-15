a3a5519ae8e55d54f4c81297ba4d07fbe6d11133
Brian Schlining <bschlining@gmail.com>
2024-08-15 09:22:50 -0700

    added tags to openapi

--- src/main/scala/org/fathomnet/worms/api/NameEndpoints.scala
+++ src/main/scala/org/fathomnet/worms/api/NameEndpoints.scala
@@ -23,142 +23,156 @@ import org.fathomnet.worms.StateController
 
 class NameEndpoints(using ec: ExecutionContext) extends Endpoints:
 
-  // -- /names
-  val namesEndpoint: PublicEndpoint[(Option[Int], Option[Int]), ErrorMsg, Page[String], Any] =
-    baseEndpoint
-      .get
-      .in("names")
-      .in(query[Option[Int]]("limit"))
-      .in(query[Option[Int]]("offset"))
-      .out(jsonBody[Page[String]])
-      .description(
-        "Returns all names ... there's a lot of names. The results are paged using query params limit (default 100) and offset (default 0)."
-      )
+    private val tag = "Names"
 
-  val namesServerEndpoint: ServerEndpoint[Any, Future] =
-    namesEndpoint.serverLogic((limitOpt, offsetOpt) =>
-      Future {
-        val limit  = limitOpt.getOrElse(100)
-        val offset = offsetOpt.getOrElse(0)
-        StateController.findAllNames(limit, offset)
-      }
+    // -- /names
+    val namesEndpoint: PublicEndpoint[(Option[Int], Option[Int]), ErrorMsg, Page[String], Any] =
+        baseEndpoint
+            .get
+            .in("names")
+            .in(query[Option[Int]]("limit"))
+            .in(query[Option[Int]]("offset"))
+            .out(jsonBody[Page[String]])
+            .description(
+                "Returns all names ... there's a lot of names. The results are paged using query params limit (default 100) and offset (default 0)."
+            )
+            .tag(tag)
+
+    val namesServerEndpoint: ServerEndpoint[Any, Future] =
+        namesEndpoint.serverLogic((limitOpt, offsetOpt) =>
+            Future {
+                val limit  = limitOpt.getOrElse(100)
+                val offset = offsetOpt.getOrElse(0)
+                StateController.findAllNames(limit, offset)
+            }
+        )
+
+    // -- /names/count
+    val namesCountEndpoint: PublicEndpoint[Unit, ErrorMsg, Int, Any] = baseEndpoint
+        .get
+        .in("names" / "count")
+        .out(jsonBody[Int])
+        .description("Returns the total number of names available.")
+        .tag(tag)
+
+    val namesCountServerEndpoint: ServerEndpoint[Any, Future] =
+        namesCountEndpoint.serverLogic(Unit => Future.successful(StateController.countAllNames()))
+
+    val namesByAphiaId: PublicEndpoint[Long, ErrorMsg, Names, Any] = baseEndpoint
+        .get
+        .in("names" / "aphiaid")
+        .in(path[Long]("aphiaid"))
+        .out(jsonBody[Names])
+        .description("Returns the data for a name given its aphiaid.")
+
+    val namesByAphiaIdServerEndpoint: ServerEndpoint[Any, Future] =
+        namesByAphiaId.serverLogic(aphiaid => Future(StateController.findNamesByAphiaId(aphiaid)))
+
+    // --/query/startswith/:prefix
+    val queryStartswithEndpoint: PublicEndpoint[String, ErrorMsg, List[String], Any] = baseEndpoint
+        .get
+        .in("query" / "startswith")
+        .in(path[String]("prefix"))
+        .out(jsonBody[List[String]])
+        .description("Returns all names that start with the given prefix.")
+        .tag(tag)
+
+    val queryStartswithServerEndpoint: ServerEndpoint[Any, Future] =
+        queryStartswithEndpoint.serverLogic(prefix => Future(StateController.queryNamesStartingWith(prefix)))
+
+    // --/query/contains/:glob
+    val queryContainsEndpoint: PublicEndpoint[String, ErrorMsg, List[String], Any] = baseEndpoint
+        .get
+        .in("query" / "contains")
+        .in(path[String]("glob"))
+        .out(jsonBody[List[String]])
+        .description("Returns all names that contain the given string (glob).")
+        .tag(tag)
+
+    val queryContainsServerEndpoint: ServerEndpoint[Any, Future] =
+        queryContainsEndpoint.serverLogic(glob => Future(StateController.queryNamesContaining(glob)))
+
+    // -- /descendants/:name
+    val descendantsEndpoint: Endpoint[Unit, (String, Option[Boolean]), ErrorMsg, List[String], Any] = baseEndpoint
+        .get
+        .in("descendants")
+        .in(path[String]("name"))
+        .in(
+            query[Option[Boolean]]("accepted")
+                .description("Only include accepted names. Allowed values are true or false. Default is false.")
+        )
+        .out(jsonBody[List[String]])
+        .description(
+            "Return the primary names of the taxa and all its descendants in alphabetical order."
+        )
+        .tag(tag)
+
+    val descendantsServerEndpoint: ServerEndpoint[Any, Future] =
+        descendantsEndpoint.serverLogic((name, accepted) =>
+            Future(StateController.descendantNames(name, accepted.getOrElse(false)))
+        )
+
+    // -- /ancestors/:name
+    val ancestorsEndpoint: PublicEndpoint[String, ErrorMsg, List[String], Any] = baseEndpoint
+        .get
+        .in("ancestors")
+        .in(path[String]("name"))
+        .out(jsonBody[List[String]])
+        .description(
+            "Return the primary names of all the ancestors in order from the top of the taxa tree down."
+        )
+        .tag(tag)
+
+    val ancestorsServerEndpoint: ServerEndpoint[Any, Future] =
+        ancestorsEndpoint.serverLogic(name => Future(StateController.ancestorNames(name)))
+
+    // -- /children/:name
+    val childrenEndpoint: PublicEndpoint[String, ErrorMsg, List[String], Any] = baseEndpoint
+        .get
+        .in("children")
+        .in(path[String]("name"))
+        .out(jsonBody[List[String]])
+        .description("Return the primary names of the direct children.")
+        .tag(tag)
+
+    val childrenServerEndpoint: ServerEndpoint[Any, Future] =
+        childrenEndpoint.serverLogic(name => Future(StateController.childNames(name)))
+
+    // -- /parent/:name
+    val parentEndpoint: PublicEndpoint[String, ErrorMsg, String, Any] = baseEndpoint
+        .get
+        .in("parent")
+        .in(path[String]("name"))
+        .out(jsonBody[String])
+        .description("Return the name of the parent of {name}.")
+        .tag(tag)
+
+    val parentServerEndpoint: ServerEndpoint[Any, Future] =
+        parentEndpoint.serverLogic(name => Future(StateController.parentName(name)))
+
+    // -- /synonyms/:name
+    val synonymsEndpoint: PublicEndpoint[String, ErrorMsg, List[String], Any] = baseEndpoint
+        .get
+        .in("synonyms")
+        .in(path[String]("name"))
+        .out(jsonBody[List[String]])
+        .description(
+            "Returns alternative names for a term. The first term in the list is the primary/accepted name."
+        )
+        .tag(tag)
+
+    val synonymsServerEndpoint: ServerEndpoint[Any, Future] =
+        synonymsEndpoint.serverLogic(name => Future(StateController.synonyms(name)))
+
+    override val all: List[ServerEndpoint[Any, Future]] = List(
+        namesServerEndpoint,
+        namesCountServerEndpoint,
+        namesByAphiaIdServerEndpoint,
+        queryStartswithServerEndpoint,
+        queryContainsServerEndpoint,
+        descendantsServerEndpoint,
+        ancestorsServerEndpoint,
+        childrenServerEndpoint,
+        parentServerEndpoint,
+        synonymsServerEndpoint
     )
-
-  // -- /names/count
-  val namesCountEndpoint: PublicEndpoint[Unit, ErrorMsg, Int, Any] = baseEndpoint
-    .get
-    .in("names" / "count")
-    .out(jsonBody[Int])
-    .description("Returns the total number of names available.")
-
-  val namesCountServerEndpoint: ServerEndpoint[Any, Future] =
-    namesCountEndpoint.serverLogic(Unit => Future.successful(StateController.countAllNames()))
-
-  val namesByAphiaId: PublicEndpoint[Long, ErrorMsg, Names, Any] = baseEndpoint
-    .get
-    .in("names" / "aphiaid")
-    .in(path[Long]("aphiaid"))
-    .out(jsonBody[Names])
-    .description("Returns the data for a name given its aphiaid.")
-
-  val namesByAphiaIdServerEndpoint: ServerEndpoint[Any, Future] =
-    namesByAphiaId.serverLogic(aphiaid => Future(StateController.findNamesByAphiaId(aphiaid)))
-
-  // --/query/startswith/:prefix
-  val queryStartswithEndpoint: PublicEndpoint[String, ErrorMsg, List[String], Any] = baseEndpoint
-    .get
-    .in("query" / "startswith")
-    .in(path[String]("prefix"))
-    .out(jsonBody[List[String]])
-    .description("Returns all names that start with the given prefix.")
-
-  val queryStartswithServerEndpoint: ServerEndpoint[Any, Future] =
-    queryStartswithEndpoint.serverLogic(prefix =>
-      Future(StateController.queryNamesStartingWith(prefix))
-    )
-
-  // --/query/contains/:glob
-  val queryContainsEndpoint: PublicEndpoint[String, ErrorMsg, List[String], Any] = baseEndpoint
-    .get
-    .in("query" / "contains")
-    .in(path[String]("glob"))
-    .out(jsonBody[List[String]])
-    .description("Returns all names that contain the given string (glob).")
-
-  val queryContainsServerEndpoint: ServerEndpoint[Any, Future] =
-    queryContainsEndpoint.serverLogic(glob => Future(StateController.queryNamesContaining(glob)))
-
-  // -- /descendants/:name
-  val descendantsEndpoint: Endpoint[Unit, (String, Option[Boolean]), ErrorMsg, List[String], Any] = baseEndpoint
-    .get
-    .in("descendants")
-    .in(path[String]("name"))
-    .in(query[Option[Boolean]]("accepted").description("Only include accepted names. Allowed values are true or false. Default is false."))
-    .out(jsonBody[List[String]])
-    .description(
-      "Return the primary names of the taxa and all its descendants in alphabetical order."
-    )
-
-  val descendantsServerEndpoint: ServerEndpoint[Any, Future] =
-    descendantsEndpoint.serverLogic((name, accepted) => Future(StateController.descendantNames(name, accepted.getOrElse(false))))
-
-  // -- /ancestors/:name
-  val ancestorsEndpoint: PublicEndpoint[String, ErrorMsg, List[String], Any] = baseEndpoint
-    .get
-    .in("ancestors")
-    .in(path[String]("name"))
-    .out(jsonBody[List[String]])
-    .description(
-      "Return the primary names of all the ancestors in order from the top of the taxa tree down."
-    )
-
-  val ancestorsServerEndpoint: ServerEndpoint[Any, Future] =
-    ancestorsEndpoint.serverLogic(name => Future(StateController.ancestorNames(name)))
-
-  // -- /children/:name
-  val childrenEndpoint: PublicEndpoint[String, ErrorMsg, List[String], Any] = baseEndpoint
-    .get
-    .in("children")
-    .in(path[String]("name"))
-    .out(jsonBody[List[String]])
-    .description("Return the primary names of the direct children.")
-
-  val childrenServerEndpoint: ServerEndpoint[Any, Future] =
-    childrenEndpoint.serverLogic(name => Future(StateController.childNames(name)))
-
-  // -- /parent/:name
-  val parentEndpoint: PublicEndpoint[String, ErrorMsg, String, Any] = baseEndpoint
-    .get
-    .in("parent")
-    .in(path[String]("name"))
-    .out(jsonBody[String])
-    .description("Return the name of the parent of {name}.")
-
-  val parentServerEndpoint: ServerEndpoint[Any, Future] =
-    parentEndpoint.serverLogic(name => Future(StateController.parentName(name)))
-
-  // -- /synonyms/:name
-  val synonymsEndpoint: PublicEndpoint[String, ErrorMsg, List[String], Any] = baseEndpoint
-    .get
-    .in("synonyms")
-    .in(path[String]("name"))
-    .out(jsonBody[List[String]])
-    .description(
-      "Returns alternative names for a term. The first term in the list is the primary/accepted name."
-    )
-
-  val synonymsServerEndpoint: ServerEndpoint[Any, Future] =
-    synonymsEndpoint.serverLogic(name => Future(StateController.synonyms(name)))
-
-  override val all: List[ServerEndpoint[Any, Future]] = List(
-    namesServerEndpoint,
-    namesCountServerEndpoint,
-    namesByAphiaIdServerEndpoint,
-    queryStartswithServerEndpoint,
-    queryContainsServerEndpoint,
-    descendantsServerEndpoint,
-    ancestorsServerEndpoint,
-    childrenServerEndpoint,
-    parentServerEndpoint,
-    synonymsServerEndpoint
-  )
--- src/main/scala/org/fathomnet/worms/api/TaxaEndpoints.scala
+++ src/main/scala/org/fathomnet/worms/api/TaxaEndpoints.scala
@@ -27,88 +27,97 @@ import sttp.tapir.server.ServerEndpoint
 
 class TaxaEndpoints(using ec: ExecutionContext) extends Endpoints:
 
-  // Make Tapir recursiive types happy
-  // https://tapir.softwaremill.com/en/latest/endpoint/schemas.html#derivation-for-recursive-types-in-scala3
-  implicit def wormsNodeSchema: Schema[WormsNode] = Schema.derived[WormsNode]
+    private val tag = "Taxa"
 
-  // -- /taxa/descendants/:name
-  /**
-   * List all descendants of a taxon.
-   */
-  val taxaDescendants: PublicEndpoint[String, ErrorMsg, WormsNode, Any] = baseEndpoint
-    .get
-    .in("taxa" / "descendants")
-    .in(path[String]("name"))
-    .out(jsonBody[WormsNode])
-    .description(
-      "Return a tree structure of descendants from the provided name on down through the tree."
+    // Make Tapir recursiive types happy
+    // https://tapir.softwaremill.com/en/latest/endpoint/schemas.html#derivation-for-recursive-types-in-scala3
+    implicit def wormsNodeSchema: Schema[WormsNode] = Schema.derived[WormsNode]
+
+    // -- /taxa/descendants/:name
+    /**
+     * List all descendants of a taxon.
+     */
+    val taxaDescendants: PublicEndpoint[String, ErrorMsg, WormsNode, Any] = baseEndpoint
+        .get
+        .in("taxa" / "descendants")
+        .in(path[String]("name"))
+        .out(jsonBody[WormsNode])
+        .description(
+            "Return a tree structure of descendants from the provided name on down through the tree."
+        )
+        .tag(tag)
+
+    val taxaDescendantsServerEndpoint: ServerEndpoint[Any, Future] =
+        taxaDescendants.serverLogic((name: String) => Future(StateController.descendantTaxa(name)))
+
+    val taxaAncestors: PublicEndpoint[String, ErrorMsg, WormsNode, Any] = baseEndpoint
+        .get
+        .in("taxa" / "ancestors")
+        .in(path[String]("name"))
+        .out(jsonBody[WormsNode])
+        .description(
+            "return a tree structure from the root of the taxonomic tree down to the given name. Note that the last node will have it's children trimmed off."
+        )
+        .tag(tag)
+
+    val taxaAncestorsServerEndpoint: ServerEndpoint[Any, Future] =
+        taxaAncestors.serverLogic((name: String) => Future(StateController.ancestorTaxa(name)))
+
+    val taxaInfo: PublicEndpoint[String, ErrorMsg, SimpleWormsNode, Any] = baseEndpoint
+        .get
+        .in("taxa" / "info")
+        .in(path[String]("name"))
+        .out(jsonBody[SimpleWormsNode])
+        .description("Returns the name, alternateNames, aphiaId, and rank of a term.")
+        .tag(tag)
+
+    val taxaInfoServerEndpoint: ServerEndpoint[Any, Future] =
+        taxaInfo.serverLogic((name: String) => Future(StateController.taxaInfo(name)))
+
+    val taxaParent: PublicEndpoint[String, ErrorMsg, SimpleWormsNode, Any] = baseEndpoint
+        .get
+        .in("taxa" / "parent")
+        .in(path[String]("name"))
+        .out(jsonBody[SimpleWormsNode])
+        .description("Returns the name, alternateNames, aphiaId, and rank of the parent of the term.")
+        .tag(tag)
+
+    val taxaParentServerEndpoint: ServerEndpoint[Any, Future] =
+        taxaParent.serverLogic((name: String) => Future(StateController.parentTaxa(name)))
+
+    val taxaChildren: PublicEndpoint[String, ErrorMsg, List[SimpleWormsNode], Any] = baseEndpoint
+        .get
+        .in("taxa" / "children")
+        .in(path[String]("name"))
+        .out(jsonBody[List[SimpleWormsNode]])
+        .description("Returns the name, alternateNames, aphiaId, and rank of the children of the term.")
+        .tag(tag)
+
+    val taxaChildrenServerEndpoint: ServerEndpoint[Any, Future] =
+        taxaChildren.serverLogic((name: String) => Future(StateController.childTaxa(name)))
+
+    val taxaQueryStartswith
+        : PublicEndpoint[(String, Option[String], Option[String]), ErrorMsg, List[SimpleWormsNode], Any] =
+        baseEndpoint
+            .get
+            .in("taxa" / "query" / "startswith")
+            .in(path[String]("prefix"))
+            .in(query[Option[String]]("rank"))
+            .in(query[Option[String]]("parent"))
+            .out(jsonBody[List[SimpleWormsNode]])
+            .description("Returns a list of taxa that start with the given prefix.")
+            .tag(tag)
+
+    val taxaQueryStartswithEndpoint: ServerEndpoint[Any, Future] =
+        taxaQueryStartswith.serverLogic((prefix: String, rank: Option[String], parent: Option[String]) =>
+            Future(StateController.taxaByNameStartingWith(prefix, rank, parent))
+        )
+
+    val all: List[ServerEndpoint[Any, Future]] = List(
+        taxaDescendantsServerEndpoint,
+        taxaAncestorsServerEndpoint,
+        taxaInfoServerEndpoint,
+        taxaParentServerEndpoint,
+        taxaChildrenServerEndpoint,
+        taxaQueryStartswithEndpoint
     )
-
-  val taxaDescendantsServerEndpoint: ServerEndpoint[Any, Future] =
-    taxaDescendants.serverLogic((name: String) => Future(StateController.descendantTaxa(name)))
-
-  val taxaAncestors: PublicEndpoint[String, ErrorMsg, WormsNode, Any] = baseEndpoint
-    .get
-    .in("taxa" / "ancestors")
-    .in(path[String]("name"))
-    .out(jsonBody[WormsNode])
-    .description(
-      "return a tree structure from the root of the taxonomic tree down to the given name. Note that the last node will have it's children trimmed off."
-    )
-
-  val taxaAncestorsServerEndpoint: ServerEndpoint[Any, Future] =
-    taxaAncestors.serverLogic((name: String) => Future(StateController.ancestorTaxa(name)))
-
-  val taxaInfo: PublicEndpoint[String, ErrorMsg, SimpleWormsNode, Any] = baseEndpoint
-    .get
-    .in("taxa" / "info")
-    .in(path[String]("name"))
-    .out(jsonBody[SimpleWormsNode])
-    .description("Returns the name, alternateNames, aphiaId, and rank of a term.")
-
-  val taxaInfoServerEndpoint: ServerEndpoint[Any, Future] =
-    taxaInfo.serverLogic((name: String) => Future(StateController.taxaInfo(name)))
-
-  val taxaParent: PublicEndpoint[String, ErrorMsg, SimpleWormsNode, Any] = baseEndpoint
-    .get
-    .in("taxa" / "parent")
-    .in(path[String]("name"))
-    .out(jsonBody[SimpleWormsNode])
-    .description("Returns the name, alternateNames, aphiaId, and rank of the parent of the term.")
-
-  val taxaParentServerEndpoint: ServerEndpoint[Any, Future] =
-    taxaParent.serverLogic((name: String) => Future(StateController.parentTaxa(name)))
-
-  val taxaChildren: PublicEndpoint[String, ErrorMsg, List[SimpleWormsNode], Any] = baseEndpoint
-    .get
-    .in("taxa" / "children")
-    .in(path[String]("name"))
-    .out(jsonBody[List[SimpleWormsNode]])
-    .description("Returns the name, alternateNames, aphiaId, and rank of the children of the term.")
-
-  val taxaChildrenServerEndpoint: ServerEndpoint[Any, Future] =
-    taxaChildren.serverLogic((name: String) => Future(StateController.childTaxa(name)))
-
-  val taxaQueryStartswith: PublicEndpoint[(String, Option[String], Option[String]), ErrorMsg, List[SimpleWormsNode], Any] =
-    baseEndpoint
-      .get
-      .in("taxa" / "query" / "startswith")
-      .in(path[String]("prefix"))
-      .in(query[Option[String]]("rank"))
-      .in(query[Option[String]]("parent"))
-      .out(jsonBody[List[SimpleWormsNode]])
-      .description("Returns a list of taxa that start with the given prefix.")
-
-  val taxaQueryStartswithEndpoint: ServerEndpoint[Any, Future] =
-    taxaQueryStartswith.serverLogic((prefix: String, rank: Option[String], parent: Option[String]) =>
-      Future(StateController.taxaByNameStartingWith(prefix, rank, parent))
-    )
-
-  val all: List[ServerEndpoint[Any, Future]] = List(
-    taxaDescendantsServerEndpoint,
-    taxaAncestorsServerEndpoint,
-    taxaInfoServerEndpoint,
-    taxaParentServerEndpoint,
-    taxaChildrenServerEndpoint,
-    taxaQueryStartswithEndpoint
-  )
